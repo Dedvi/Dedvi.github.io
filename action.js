277448c2537730a57854b49ed0154e4f09668adf
Dedvi <63316469+Dedvi@users.noreply.github.com>
2020-04-08 23:42:24 -0400

    updated the animations for welcome page

--- action.js
+++ action.js
@@ -2,13 +2,13 @@ var click=0;
 function reduce() {
     if(click==0){
     var a = document.getElementById("welcome");
-    var top=150;
-    a.style.fontSize='200%';
+    var top=100;
+    a.style.fontSize='1.69vw';
     a.style.top='20%';
     a.style.bottom='80%';
     a.style.left='47%';
     a.style.right='45%';
-    a=document.getElementById("main");
+    a=document.getElementById("links");
     var id = setInterval(frame, 5);
     function frame() {
         if (top == 20) {
